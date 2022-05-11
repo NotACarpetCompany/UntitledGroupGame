10cbb71d66d94dbef9e38316a9a9d0f27235d824
Rafael Edora <rvedora@gmail.com>
2022-05-11 16:55:45 -0700

    initial commit

---
+++ TheTaxCollector/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{   
+    public float speed = 5;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Looks for right / left input
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        // Looks for up / down input
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+    }
+
+    // FixedUpdate is called at a fixed interval
+    void FixedUpdate () {
+
+        // Move character
+        float horDist = horizontalMove * speed * Time.fixedDeltaTime;
+        float vertDist = verticalMove * speed * Time.fixedDeltaTime;
+        Move(horDist, vertDist);
+    }
+
+    void Move (float horizontalDist, float verticalDist) {
+
+        Vector3 totalMove  = new Vector3 (horizontalDist, verticalDist, 0);
+        transform.position = transform.position + totalMove;
+    }
+}
